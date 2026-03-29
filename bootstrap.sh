5ab0dcd912d235a5c6edfdde02610a25a7b5ac5f
Trevor Lauder <trevor@lauder.family>
2026-03-28 23:20:02 -0600

    Update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-brew install chezmoi
+brew install chezmoi \
+  starship \
+  fzf \
+  zsh-syntax-highlighting \
+  zsh-autosuggestions \
+  zsh-git-prompt
+
 chezmoi init https://github.com/trevorlauder/dotfiles
 chezmoi apply
