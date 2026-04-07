6a0e6b155f0558d5b4bc947b1f492ead7335f708
Trevor Lauder <trevor.l@getjobber.com>
2026-04-07 15:30:00 -0600

    Update bootstrap.sh to clone zgen and init chezmoi
    Clone zgen repository and initialize chezmoi with dotfiles.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@
 
 mkdir -p ${HOME}/.local/bin
 
+git clone --depth 1 https://github.com/tarjoilija/zgen.git "${HOME_DIR}/.zgen"
+rm -rf "${HOME_DIR}/.zgen/.git"
+
 mise install -y
 
 chezmoi init https://github.com/trevorlauder/dotfiles
