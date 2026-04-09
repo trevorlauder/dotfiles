331cfb6cc7cf551826c91e91958de47da19550ba
Trevor Lauder <trevor.l@getjobber.com>
2026-04-09 23:38:09 +0000

    Add chmod for zgen to zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,6 @@ mkdir -p ${HOME}/.local/bin
 
 git clone --depth 1 https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 rm -rf "${HOME}/.zgen/.git"
-chmod -R go-w "${HOME}/.zgen"
 
 mise install -y
 
