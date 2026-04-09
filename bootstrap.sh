c9f8292af76d4c1755458805021dc50b4529e3d4
Trevor Lauder <trevor.l@getjobber.com>
2026-04-09 23:34:13 +0000

    chmod zgen

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,9 @@
 
 mkdir -p ${HOME}/.local/bin
 
-umask 0022
 git clone --depth 1 https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 rm -rf "${HOME}/.zgen/.git"
+chmod -R go-w "${HOME}/.zgen"
 
 mise install -y
 
