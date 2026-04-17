857f2c8e03767a11d6eb6578e515111f94ab84f1
Trevor Lauder <trevor.l@getjobber.com>
2026-04-17 10:20:18 -0600

    Update chezmoi

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,7 @@ mkdir -p ${HOME}/.local/bin
 git clone --depth 1 https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 rm -rf "${HOME}/.zgen/.git"
 
-eval "$(mise activate bash)"
 mise install chezmoi
 
-chezmoi init https://github.com/trevorlauder/dotfiles
-chezmoi apply
+mise exec -- chezmoi init https://github.com/trevorlauder/dotfiles
+mise exec -- chezmoi apply
