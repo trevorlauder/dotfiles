9466696aeab1eee4be8267176a661f4ad93500e9
Trevor Lauder <trevor.l@getjobber.com>
2026-04-17 10:14:56 -0600

    Add mise trust

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,10 @@ mkdir -p ${HOME}/.local/bin
 git clone --depth 1 https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 rm -rf "${HOME}/.zgen/.git"
 
+mise trust
+
 eval "$(mise activate bash)"
+
 mise install -y
 
 chezmoi init https://github.com/trevorlauder/dotfiles
