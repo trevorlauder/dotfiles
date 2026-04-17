dbb417c8ec58684bbea33109301ff0169f3251e2
Trevor Lauder <trevor.l@getjobber.com>
2026-04-17 10:41:16 -0600

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,9 @@ mkdir -p ${HOME}/.local/bin
 git clone --depth 1 https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 rm -rf "${HOME}/.zgen/.git"
 
-mise use -g chezmoi
+mise use -p ${HOME}/.config/mise/conf.d/000-bootstrap.toml chezmoi
 
 mise exec -- chezmoi init https://github.com/trevorlauder/dotfiles
 mise exec -- chezmoi apply
 
-mise unuse -p ${HOME}/.config/mise/conf.d/999-base.toml chezmoi
+mise unuse -p ${HOME}/.config/mise/conf.d/000-bootstrap.toml chezmoi
