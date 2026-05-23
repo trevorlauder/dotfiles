d0f9f586257afd617bcf5230b2221fd89dcb77bd
Trevor Lauder <me@trevorlauder.dev>
2026-05-23 11:35:07 -0600

    Add mise commands to bootstrap script
    Signed-off-by: Trevor Lauder <me@trevorlauder.dev>

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,8 @@ rm -rf "${HOME}/.zgen"
 git clone --depth 1 https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 rm -rf "${HOME}/.zgen/.git"
 
+mise trust ~/.dotfiles
+
 mise use -p ${HOME}/.config/mise/conf.d/000-bootstrap.toml chezmoi
 
 mise exec -- chezmoi init https://github.com/trevorlauder/dotfiles
